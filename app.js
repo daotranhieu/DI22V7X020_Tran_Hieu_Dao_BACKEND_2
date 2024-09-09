f7e8d4173c7747cc84e7571173dab652ef323a55
Chuong tran <Tranchuong@gmail.com>
2024-09-09 22:20:52 +0700

    api backend contact

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('express');
+const morgan = require('morgan');
+const app = express("cors");
+const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.routes");
+
+//morgan
+app.use(morgan('combined'));
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({message: err.message || "Internal Server Error",});
+});
+    
+
+module.exports = app;
